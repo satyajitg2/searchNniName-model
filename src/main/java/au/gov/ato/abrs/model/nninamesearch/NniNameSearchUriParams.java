13eb6f1cc9637af6846a02cb70838234f926a10c
Johnathan Ingram <jingram@moxie.digital>
2021-08-20 13:27:03 +0930

    Added getters and setters so that PropertyBindingSupport in camel can set private properties Enhanced regex on orgName to allow lowercase a-z .. it then uppercases all on the getter (more human friendly)

--- src/main/java/au/gov/ato/abrs/model/nninamesearch/NniNameSearchUriParams.java
+++ src/main/java/au/gov/ato/abrs/model/nninamesearch/NniNameSearchUriParams.java
@@ -21,11 +21,62 @@ public class NniNameSearchUriParams extends TechArchHeader {
     @Pattern(regexp = "^[123ABCEGHIJLMNOPRSTX]$", message="Invalid scope defined")
     private String scope;
 
-    @Pattern(regexp = "^[A-Z0-9 .,?!(){}:;'\"@#$%*=&|\\-_\\\\/]*$", message = "organisation name invalid'")
+    @Pattern(regexp = "^[a-zA-Z0-9 .,?!(){}:;'\"@#$%*=&|\\-_\\\\/]*$", message = "organisation name invalid'")
     private String orgName;
+
     private NniTypeCodeType orgType;
     private SearchStatusType orgStatus;
 
     @Max(value = 99, message = "A maximum of 99 records can only be returned.")
     private   int maxResults;
+
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getScope() {
+        return this.scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getOrgName() {
+        return this.orgName.toUpperCase();
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public NniTypeCodeType getOrgType() {
+        return this.orgType;
+    }
+
+    public void setOrgType(NniTypeCodeType orgType) {
+        this.orgType = orgType;
+    }
+
+    public SearchStatusType getOrgStatus() {
+        return this.orgStatus;
+    }
+
+    public void setOrgStatus(SearchStatusType orgStatus) {
+        this.orgStatus = orgStatus;
+    }
+
+    public int getMaxResults() {
+        return this.maxResults;
+    }
+
+    public void setMaxResults(int maxResults) {
+        this.maxResults = maxResults;
+    }
+
 }

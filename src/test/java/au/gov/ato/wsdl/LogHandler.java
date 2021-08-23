c0c3c266e02a770974c22d9a820606c0e16a2fd9
Johnathan Ingram <jingram@moxie.digital>
2021-08-23 16:56:27 +0930

    Forced WSDL mapping to use Strings instead of enums for XSD enumerations Updated NniNameSearchUriParams   - Uses string to replace enum value   - Added regex to do validation allowing single bean validation for optional enums and other items

--- src/test/java/au/gov/ato/wsdl/LogHandler.java
+++ src/test/java/au/gov/ato/wsdl/LogHandler.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.xml.namespace.QName;
 import javax.xml.soap.SOAPException;
